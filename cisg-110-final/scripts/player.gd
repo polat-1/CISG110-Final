2dbba82ecd877d1dbfd4972806c4bb5b4bfd30a5
polat-1 <thepolaris27@gmail.com>
2026-05-10 09:40:34 -0700

    y

--- cisg-110-final/scripts/player.gd
+++ cisg-110-final/scripts/player.gd
@@ -1,10 +1,18 @@
 extends CharacterBody2D
 
-
 @export var SPEED = 0
 @export var JUMP_VELOCITY = 0
 
+@export var MAX_KICK_TIME = 0.0
+@export var KICK_TIMER = 0.0
+
 @export var _kickRight: Node2D
+@export var _kickLeft: Node2D
+
+@export var _rightKickDir = Vector2.RIGHT
+@export var _leftKickDir = Vector2.LEFT
+
+var _facingRight: bool = true
 
 func _enter_tree() -> void:
 	_disableKick()
@@ -26,14 +34,38 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	if direction > 0:
+		_facingRight = true
+	elif direction < 0:
+		_facingRight = false
+
+	if KICK_TIMER > 0:
+		KICK_TIMER -= delta
+	if KICK_TIMER < 0:
+		_disableKick()
+
 	if Input.is_action_just_pressed("ui_accept"):
 		_kick()
 
 	move_and_slide()
 
 func _kick() -> void:
-	print("player kick")
-	_kickRight.disabled = false
+	if _facingRight:
+		_kickRight.process_mode = Node.PROCESS_MODE_INHERIT
+	else:
+		_kickLeft.process_mode = Node.PROCESS_MODE_INHERIT
+	KICK_TIMER = MAX_KICK_TIME
 	
 func _disableKick() -> void:
-	_kickRight.disabled = true
+	_kickRight.process_mode = Node.PROCESS_MODE_DISABLED
+	_kickLeft.process_mode = Node.PROCESS_MODE_DISABLED
+
+func _on_kick_right_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	print("kick right")
+	print(body.name)
+	body.apply_central_impulse(_rightKickDir)
+
+func _on_kick_left_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	print("kick left")
+	print(body.name)
+	body.apply_central_impulse(_leftKickDir)
